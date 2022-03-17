6617d490c85eb32ddfe7ada6bb32602e18a8dc3f
unknown <krish>
2022-03-17 16:51:41 +0530

    fourth commit

--- nodejs/index.js
+++ nodejs/index.js
@@ -8,6 +8,6 @@ app.get('/',(req,res)=>{
     res.end("Server Started");
 });
 
-app.listen(process.env.PORT|8000,(err)=>{
+app.listen(process.env.PORT||8000,(err)=>{
     console.log("server listining on port 8000");
 });
