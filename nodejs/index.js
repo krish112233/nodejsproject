7efa39ff2f886494eef824762407ad0e527007d2
unknown <krish>
2022-03-17 16:45:59 +0530

    third commit

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,1 +1,13 @@
-console.log("server created");
+const express=require('express');
+
+
+const app=express();
+
+app.get('/',(req,res)=>{
+
+    res.end("Server Started");
+});
+
+app.listen(process.env.PORT||8000,(err)=>{
+    console.log("server listining on port 8000");
+});
